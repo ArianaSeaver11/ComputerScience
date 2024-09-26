289e4318ece6d9e03c4703d1d86637b844a7d328
ArianaSeaver11 <824760@mystma.org>
2024-09-26 15:13:13 -0500

    mad_lib_sept26

--- Assignments/AminalSurvey.py
+++ Assignments/AminalSurvey.py
@@ -1,6 +1,6 @@
-favorite_animal = input("What is your favorite animal?\n>")
-color = input("What color are they?\n>")
-live = input("Where do they live?\n>")
+favorite_animal = input("What is your favorite animal?\n>") #
+color = input("What color are they?\n>") # there can be more then one color
+live = input("Where do they live?\n>") # just a description 
 fact = input("What is a fun fact about them?\n>")
 sound = input("What sound so they make?\n>")
-print("Your favorite animal are" + favorite_animal + " ." + "They are " +  color + " ." + "They live in " +  live + " ," + " and a fun fact about them is " +  fact + " " + ". They make  " +  sound + " ")
+print("Your favorite animal are " + favorite_animal + "." + " " + They are " +  color + "." + "They live in " +  live + "," + " and a fun fact about them is " +  fact + "." + "They make " +  sound + " ")
