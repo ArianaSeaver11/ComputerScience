a282b535ca2b2ee6c5dd224041812ca7e224389e
ArianaSeaver11 <824760@mystma.org>
2024-09-27 14:25:17 -0500

    mab_lib_sept27

---
+++ Assignments/mad_lib.py
@@ -0,0 +1,22 @@
+
+#Once upon a time, in a cold forest
+#there lived a small cat
+#everyday, the cat would walk slowly
+# one day, a old bear appearded
+# and chased the cat away
+#The cat was tired and scared
+adjective = input("enter adjective\n>")
+noun = input("enter noun\n>")
+#Once upon a time, in a cold forest
+adjective = input("enter adjective\n>")
+noun = input("enter noun\n>")
+#there lived a small cat
+noun = input("Enter noun and adverb\n> ")
+#everyday, the cat would walk slowly
+new_noun = input("enter noun\n>")
+# one day, a old bear appearded
+chased_cat = input(" enter action\n>")
+# and chased the cat away
+tired_scared = input(" enter Emotion\n>")
+#The cat was tired and scared
+print("Once upon a time in a " + adjective + noun + "." + "There lived a " + adjective + noun + "." + "Every day they would " + noun + "." +  "On day a " + new_noun + " " + "and chased the " + chased_cat + "." + "The animal was " + tired_scared + ".")
---
+++ Unit 2 - Functions and Variables/data_conversion.py
@@ -0,0 +1,31 @@
+# Start by copying this code block into a new file called dataconversion.py in your unit 2 folder. This is an ungraded activity.
+
+# Successfully convert all of the following variables to another type and print the result
+# If the conversion prints without errors, you did the conversion correctly
+
+a = 115         #int -> string
+a = str(a)
+
+b = 3.14        #float -> string
+b = str(b)
+
+c = "123"        #string -> int
+num = int(c)
+
+d = "True"      #string -> boolean
+bool(d)
+e = True      #boolean -> string
+e = str(e)
+f = False       
+f = str(f)
+#boolean -> string
+g = '10110111'  #byte -> int
+g = int(g)
+h = "2.54"      #string -> float
+h = float(h)
+i = 100         #int -> float
+i = float(i)
+j = 10.0        #float -> int
+j = int(j)
+k = 254         #int -> byte
+k = int(k)
