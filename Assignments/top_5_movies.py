90f625caa32fcb72e8c5aa4a2056bfb0e096afbf
ArianaSeaver11 <824760@mystma.org>
2024-09-30 14:29:51 -0500

    top_5_movies

---
+++ Assignments/top_5_movies.py
@@ -0,0 +1,9 @@
+def good_movies(movie1, movie2, movie3, movie4, movie5):
+    print("My top 5 movies")
+    print("1. Antman")
+    print("2. New Groove")
+    print("3. cars")
+    print("4. cars 2")
+    print("5. cars 3")
+
+good_movies("Cars", "Cars 2," "Cars 3", "new groove", "antman")
