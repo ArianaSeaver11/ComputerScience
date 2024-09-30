02006500e2003797d83195f2ad3d70a6bdd5c66a
ArianaSeaver11 <824760@mystma.org>
2024-09-30 14:21:09 -0500

    top_5_movies

---
+++ Unit 2 - Functions and Variables/functions.py
@@ -0,0 +1,26 @@
+x = 5
+
+def my_function():     #define the function with a nmae
+    print("Hello World")
+    print("catdog")
+
+print("Osowski r00lz d00d")
+my_function()
+
+def good_movies(movie1, movie2, movie3, movie4, movie5):
+    print("My top 5 movies")
+    print("1. Antman")
+    print("2. New Groove")
+    print("3. cars")
+    print("4. cars 2")
+    print("5. cars 3")
+    
+good_movies()
+
+def add(x, y): 
+    print(x + y)
+
+add(5, 10)
+add (2, 2)
+add(33, 33)
+add("cat", "dog")
---
+++ Unit 2 - Functions and Variables/top_5_movies.py
@@ -0,0 +1,18 @@
+def good_movies(movie1, movie2, movie3, movie4, movie5):
+    print("My top 5 movies")
+    print("1. " + movie1)
+    print("2. " + movie2)
+    print("3. " + movie3)
+    print("4. " + movie4)
+    print("5. " + movie5)
+    
+good_movies("Cars", "Cars 2," "Cars 3", "new groove", "antman")
+
+def add(x, y): 
+    print(x + y)
+
+add(5, 10)
+add (2, 2)
+add(33, 33)
+add("cat", "dog")
+
