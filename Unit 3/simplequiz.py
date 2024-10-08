d8b830ebc49ffe2c4f5c3f059c7cfcdc9f6e5dfe
ArianaSeaver11 <824760@mystma.org>
2024-10-08 14:47:39 -0500

    simple_quiz

---
+++ Unit 3/simplequiz.py
@@ -0,0 +1,23 @@
+score = 0
+one = input("What is a number greater than 4 and less than 6?\n>")
+if one == "5":
+    score = score + 1
+
+two = input("a number equal to 35?\n>")
+if two == "35":
+    score = score + 1
+
+three = input("How old is santa?\n>")
+if three == "old":
+    score = score + 1
+
+four = input("Best color?\n>")
+if four == "green":
+    score = score + 1
+
+five = input("What's the color of the sky?\n>")
+if five == "blue":
+    score = score + 1
+
+print(score)
+
