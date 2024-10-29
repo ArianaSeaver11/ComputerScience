50764fbece832468dcd7434dca7ba18921292aa2
ArianaSeaver11 <824760@mystma.org>
2024-10-29 14:46:51 -0500

    oct_29

--- Tests/textadventure.py
+++ Tests/textadventure.py
@@ -18,7 +18,7 @@ def start_adventure():      # Getting ingredients to make a cake at Walmart
 
 
 def enter_walmart():
-   print("You start the adventure by walking into the store where should you start...")
+   print("You start the adventure by walking into the store, where should you start...")
 
 def go_home():
     print("You decide to go back home, your mother is upset that there is no cake and kicks you out of the house.")     # first ending
@@ -27,7 +27,7 @@ def go_home():
 
 start_adventure()
 
-def first_aisle():
+def first_aisle():      #start finding the flower
    print("You need to start collecting your ingredients. Do you:")
    print("1. Start in the candy aisle")
    print("2. Start in the baking aisle")
@@ -51,7 +51,7 @@ def first_aisle():
 
 def enter_candy():
    print("You decide that the cake can wait, you're starving!")
-   print("Because you bought candy,you end up getting diabetes, causing you many health problems.")       #2nd ending??
+   print("Because you bought candy, you end up getting diabetes, causing you many health problems.")       #2nd ending??
    exit()
 
 
@@ -64,7 +64,7 @@ def enter_furniture():
 
 first_aisle()
 
-def finding_baking_soda():
+def finding_baking_soda():      #finding baking soda
    print("But you realize you can't find the baking soda. Do you:")
    print("1. Ask for help")
    print("2. Keep looking ")
@@ -79,7 +79,7 @@ def finding_baking_soda():
    elif choice == "3":
        give_up()
    else:
-       print("Try again.")
+       print("Invalid choice. Try again")
        finding_baking_soda()
 
 
@@ -92,12 +92,12 @@ def keep_looking():
 
 
 def give_up():
-   print("You give up and just go home, your mother is very disappointed")      # 3rd ending
+   print("You give up and just go home, your mother is very disappointed.")      # 3rd ending
    exit()
 
 finding_baking_soda()
 
-def strange_sound():
+def strange_sound():        #might be a karen attack
    print("You need to continue collecting ingredients, but you hear a strange sound. Do you:")
    print("1. Go a different direction")
    print("2. Continue anyway")
@@ -111,7 +111,7 @@ def strange_sound():
        unexpented_attack()
 
    else:
-       print("Try again.")
+       print("Invalid choice. Try again")
        strange_sound()
 
 def leave():
@@ -129,7 +129,7 @@ def karen_attack():
 
 strange_sound()
 
-def finding_the_butter():
+def finding_the_butter():           #collecting butter
    print("You have 2 ingredients left to collect. Where should you go next:")
    print("1. The office and art department")
    print("2. The dairy section")
@@ -150,7 +150,7 @@ def finding_the_butter():
        finding_the_butter()
 
 def enter_office():
-    print("You honeslty don't know way you came this way, you turn back and go to the dairy department.")
+    print("You honeslty don't know why you came this way, you turn back and go to the dairy department.")
     enter_dairy()
 
 def enter_dairy():
@@ -164,7 +164,7 @@ def enter_electronics():
 finding_the_butter()
 
 
-def weird_creature():
+def weird_creature():       #it's Gale Luis, either you get it or you don't
     print("You feel like something or someone is watching you...")
     print("1. Look for this creature")
     print("2. Ignore it ")
@@ -203,7 +203,7 @@ def enter_starvation():
 
 weird_creature()
 
-def poultry_section():
+def poultry_section():          # finding the eggs
     print("The only thing left to find is the eggs. Do you:")
     print("1. Start on the left side of aisle.")
     print("2. Start on the right side of the aisle.")
@@ -229,7 +229,7 @@ def enter_right_side():
 
 poultry_section()
 
-def check_out():
+def check_out():            # buying the ingredients
     print("You finally have the items collected. Do you:")
     print("1. Self checkout ")
     print("2. Get in the other checkout lanes.")
@@ -247,13 +247,13 @@ def check_out():
 
 def  enter_self_chechkout():
     print("You are able to pay for everything and are able to leave the store.")
-    print("You are able to make your mom a cake, so you lets you to continue to live in her basement.")
+    print("You bake your mom a cake and she excepts your offering, and no longer wants to kick you out of the house.")
     enter_the_end()
 
 def enter_checkout_line():
-    print("You have to wait in a long line, and are finally able to leave.")
-    print("You bake your mom a cake and she excepts your offering, and no longer wants to kick you out of the house.")
-    enter_the_end()
+    print("What kind of psycho doesnt go to self checkout, go back…")
+    check_out()
+
 
 def enter_the_end():
     print("You finished!! You successfully completed the task.")
